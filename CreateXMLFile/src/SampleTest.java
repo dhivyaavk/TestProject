38506ec92e7011fbb04bcb8e923f39d3dbc6f8c7
qtp0111 <qtp0111@TZW1108-L1.liberty.fin-za.net>
2019-11-26 14:09:57 +0200

    Version2 Commit

--- CreateXMLFile/src/SampleTest.java
+++ CreateXMLFile/src/SampleTest.java
@@ -2,8 +2,14 @@
 public class SampleTest {
 
 	public static void main(String[] args) {
+		SampleTest tst = new SampleTest();
 		System.out.println("Test1");
+		tst.test();
 
+	}
+	
+	public void test(){
+		System.out.println("Hello world");
 	}
 
 }
